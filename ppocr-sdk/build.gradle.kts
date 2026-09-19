447cbe06849d1760de361b4bc6641042bb47ed16
KAN1409 <engkareemnasser@gmail.com>
2026-09-19 12:25:19 +0300

    build: align PaddleOCR Java target with Kotlin target

--- ppocr-sdk/build.gradle.kts
+++ ppocr-sdk/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     defaultConfig { minSdk = 24 }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlin { compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11) } }
 }
